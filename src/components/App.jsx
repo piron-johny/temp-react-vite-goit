d28792d7edef5ca3155fd180209ea0c0b88df656
piron-johny <piron.johny@gmail.com>
2023-08-04 13:37:11 +0300

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          height: '100vh',
+          width: '100vw',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          textTransform: 'uppercase',
+          color: '#ff6c00',
+          fontWeight: 700,
+        }}
+      >
+        React
+        <span
+          style={{
+            display: 'inline-block',
+            margin: '0 10px',
+            padding: '0px 10px',
+            background: '#fff',
+            color: '#000',
+            borderRadius: '3px',
+            border: '1px solid #eee',
+          }}
+        >
+          homework
+        </span>
+        template
+      </div>
+    </>
+  );
+}
+
+export default App;

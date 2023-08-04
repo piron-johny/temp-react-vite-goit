9671009bac67d053024c78aa8ad521431b7cd63f
piron-johny <piron.johny@gmail.com>
2023-08-04 17:06:23 +0300

    upd vite conf & creating a router

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// eslint-disable-next-line no-undef
+const isDev = process.env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/temp-react-vite-goit/',
-})
+  base: isDev ? '/' : '/temp-react-vite-goit/',
+});

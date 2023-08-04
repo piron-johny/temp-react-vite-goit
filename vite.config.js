85ff0717893f0b95e0793a019e1ff3499add2ddc
piron-johny <piron.johny@gmail.com>
2023-08-04 17:25:25 +0300

    upd vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// eslint-disable-next-line no-undef
-const isDev = process.env.NODE_ENV === 'development';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isDev ? '/' : '/temp-react-vite-goit/',
+  base: '/temp-react-vite-goit/',
 });
